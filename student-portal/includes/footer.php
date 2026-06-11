a2fb91f641fefae4aa15a6b7937b07114ab861f9
awale6732 <abdirahmanabdirahmanbadhe@gmail.com>
2026-06-11 09:10:48 -0700

    Delete student-portal directory

--- student-portal/includes/footer.php
+++
@@ -1,8 +0,0 @@
-    </div>
-    <footer class="footer">
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> BIT3208 - Advanced Web Design and Development. All rights reserved.</p>
-        </div>
-    </footer>
-</body>
-</html>
